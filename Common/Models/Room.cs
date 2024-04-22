5f254b820ec8717e644fc654daa4665f51894f6d
Mykola V <nikola@rootnix.net>
2024-04-22 16:53:46 +0200

    feat: room synchronous sending messages

--- Common/Models/Room.cs
+++ Common/Models/Room.cs
@@ -3,6 +3,10 @@ using vut_ipk2.Common.Managers;
 
 namespace vut_ipk2.Common.Models;
 
+/// <summary>
+/// Object representing a chat room.
+/// Implements IAsyncObservable to allow for subscribing and unsubscribing to messages.
+/// </summary>
 public class Room : IAsyncObservable<MessageInfo>
 {
     public string Name { get; }
@@ -14,10 +18,8 @@ public class Room : IAsyncObservable<MessageInfo>
         Name = name;
     }
     
-    public async Task SubscribeAsync(IAsyncObserver<MessageInfo> observer)
-    {
+    public async Task SubscribeAsync(IAsyncObserver<MessageInfo> observer) =>
         _observers.Add(observer);
-    }
     
     public async Task UnsubscribeAsync(IAsyncObserver<MessageInfo> observer)
     {
@@ -27,6 +29,12 @@ public class Room : IAsyncObservable<MessageInfo>
             RoomManager.RemoveRoom(this);
     }
     
+    /// <summary>
+    /// Notify all observers about the new message.
+    /// Don't notify the observer who sent the message, if message is server's.
+    /// </summary>
+    /// <param name="observerWhoSend">Observer who sent the message</param>
+    /// <param name="value">Message</param>
     public async Task NotifyAsync(IAsyncObserver<MessageInfo> observerWhoSend, MessageInfo value)
     {
         foreach (var observer in _observers)
@@ -34,7 +42,7 @@ public class Room : IAsyncObservable<MessageInfo>
             if (observer == observerWhoSend && value.From != "Server")
                 continue;
             
-            Task.Run(() => observer.OnNextAsync(value));
+            await observer.OnNextAsync(value);
         }
     }
 }
